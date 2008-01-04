4ff47d41be8f2f06a00d1a2db80370a317bfe8bb
Digger Vermont <digVer@verizon.net>
2008-01-04 04:05:04 +0000

    changed database user to mess

--- mess/settings.py
+++ mess/settings.py
@@ -11,7 +11,7 @@ MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'mess'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'mariposa'             # Not used with sqlite3.
+DATABASE_USER = 'mess'             # Not used with sqlite3.
 DATABASE_PASSWORD = 'caGlisjem6'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
