8f92f772aee1e51d50892d185bc2300ed5eb1868
Digger Vermont <digVer@verizon.net>
2008-01-12 18:58:09 +0000

    Added an Admin class. Added null values to integer fields. Added blank to several fields Changed __str__ to not use smart_str since I'm using Django 0.96

--- mess/contacts/models.py
+++ mess/contacts/models.py
@@ -4,25 +4,37 @@ class Email(models.Model):
     email = models.EmailField(unique=True)
 
     def __str__(self):
-        return smart_str('%s' % (self.email))
+        return '%s' % (self.email)
+
+    class Admin:
+        pass
+
 
 class Phone(models.Model):
     number = models.PhoneNumberField(unique=True)
-    extension = models.PositiveIntegerField(maxlength=5)
+    extension = models.PositiveIntegerField(maxlength=5, null=True, blank=True)
 
     def __str__(self):
-        if self.extention:
-            return smart_str('%s ext: %s' % (self.number, self.extention))
+        if self.extension:
+            return '%s ext: %s' % (self.number, self.extension)
         else:
-            return smart_str('%s' % (self.number))
+            return '%s' % (self.number)
+
+    class Admin:
+        pass
+
 
 class Address(models.Model):
     address_1 = models.CharField(maxlength=40)
     address_2 = models.CharField(maxlength=40, blank=True)
     city = models.CharField(maxlength=40)
     state = models.USStateField()
-    zip_1 = models.PositiveIntegerField(maxlength=5)
-    zip_2 = models.PositiveIntegerField(maxlength=4)
+    zip_1 = models.PositiveIntegerField(maxlength=5, null=True, blank=True)
+    zip_2 = models.PositiveIntegerField(maxlength=4, null=True, blank=True)
     
     def __str__(self):
-        return smart_str('%s' % (self.address_1))
+        return '%s' % (self.address_1)
+
+    class Admin:
+        pass
+
