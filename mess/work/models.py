7c7b548e329979b7963acf5dd1a2489791b8a8af
Digger Vermont <digVer@verizon.net>
2008-01-12 18:59:05 +0000

    Added Meta and Admin class

--- mess/work/models.py
+++ mess/work/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 class Job(models.Model):
-    name = models.CharField(maxlength=40)
+    name = models.CharField(maxlength=40, unique=True)
 
     def __str__(self):
-        return smart_str('%s' % (self.name))
+        return '%s' % (self.name)
+    
+    class Meta:
+        ordering = ['name']
+
+    class Admin:
+        pass
 
