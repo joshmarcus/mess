b79f87e6cf65dbe870f5bb6e6dead3433870a6c9
Digger Vermont <digVer@verizon.net>
2008-01-20 23:43:08 +0000

    Removed user, pass, and role.  Seems we can do this through the admin section.
    Removed the Email and Phone models.  Removed the ForeignKey's for email and phone in Member.  Added fields to to the Member model for email and phone.
    
    Changed the status field.
    
    Other changes?

--- mess/membership/models.py
+++ mess/membership/models.py
@@ -5,37 +5,13 @@ from django.db import models
 from mess.work.models import Job
 
 
-class Email(models.Model):
-    email = models.EmailField(unique=True)
-
-    def __str__(self):
-        return '%s' % (self.email)
-
-    class Admin:
-        pass
-
-
-class Phone(models.Model):
-    number = models.PhoneNumberField(unique=True)
-    extension = models.PositiveIntegerField(maxlength=5, null=True, blank=True)
-
-    def __str__(self):
-        if self.extension:
-            return '%s ext: %s' % (self.number, self.extension)
-        else:
-            return '%s' % (self.number)
-
-    class Admin:
-        pass
-
-
 class Address(models.Model):
-    address_1 = models.CharField(maxlength=40)
-    address_2 = models.CharField(maxlength=40, blank=True)
-    city = models.CharField(maxlength=40, default='Philadelphia')
+    address_1 = models.CharField(max_length=40)
+    address_2 = models.CharField(max_length=40, blank=True)
+    city = models.CharField(max_length=40, default='Philadelphia')
     state = models.USStateField(default='PA')
-    zip_1 = models.PositiveIntegerField(maxlength=5, null=True, blank=True)
-    zip_2 = models.PositiveIntegerField(maxlength=4, null=True, blank=True)
+    zip_1 = models.PositiveIntegerField(max_length=5, null=True, blank=True)
+    zip_2 = models.PositiveIntegerField(max_length=4, null=True, blank=True)
     
     def __str__(self):
         return '%s' % (self.address_1)
@@ -43,59 +19,19 @@ class Address(models.Model):
     class Admin:
         pass
 
-'''
-class Attribute(models.Model):
-
-    LOCATION_CHOICES = (
-        ('u','Unknown'),
-        ('h','Home'),
-        ('w','Work'),
-        ('m','Mobile'),
-        ('o','Other'),
-    )
-
-    public = models.BooleanField()
-    location = models.CharField(maxlength=1, default='u')
-
-
-class EmailAttribute(Attribute):
-    email = models.ForeignKey(Email)
-    member = models.ForeignKey('Member')
-
-
-class PhoneAttribute(Attribute):
-    
-    TYPE_CHOICES = (
-        ('u','Unknown'),
-        ('v','Voice'),
-        ('m','Voice Mail'),
-        ('p','Pager'),
-        ('f','Fax'),
-    )
-
-    phone = models.ForeignKey()
-    member = models.ForeignKey('Member')
-    type = models.CharField(maxlength=1, default='u')
-
-
-class AddressAttribute(Attribute):
-    email = models.ForeignKey(Email)
-    member = models.ForeignKey('Member')
-'''
 
 class Account(models.Model):
 
     def balance():
-        return b
+        return balance
 
-    name = models.CharField(maxlength=40, unique=True)
+    name = models.CharField(max_length=40, unique=True)
     contact = models.ForeignKey('Member', related_name='Member.account',
                                 blank=True, null=True)
-    #balance = models.DecimalField(max_digit=4, decimal_places=2,
-    #                            blank=True, null=True)
-    balance = models.IntegerField(default=0, blank=True, null=True)
+    balance = models.DecimalField(max_digits=4, decimal_places=2,
+                                blank=True, null=True)
 
-    def __str__(self):
+    def __unicode__(self):
         return '%s' % (self.name)
     
     class Meta:
@@ -104,15 +40,9 @@ class Account(models.Model):
     class Admin:
         pass
 
+
 class Member(models.Model):
 
-    ROLE_CHOICES = (
-        ('g', 'Guest'),
-        ('m', 'Member'),
-        ('c', 'Cashier'),
-        ('s', 'Staff'),
-    )
-    
     STATUS_CHOICES = (
         ('a', 'Active'),
         ('w', 'Working'),  # Member is active and has a job.
@@ -127,45 +57,116 @@ class Member(models.Model):
         ('e', 'Email'),
         ('t', 'Telephone'),
     )
-    
-    user = models.CharField(maxlength=20, blank=True, unique=True)
-    password = models.CharField(maxlength=96, blank=True)
-    role = models.CharField(maxlength=1, choices=ROLE_CHOICES,
-                            default='m', radio_admin=True)
-    given = models.CharField(maxlength=20, blank=True)
-    middle = models.CharField(maxlength=20, blank=True)
-    family = models.CharField(maxlength=20, blank=True)
-    status = models.CharField(maxlength=1, choices=STATUS_CHOICES,
+
+    LOCATION_CHOICES = (
+        ('u','Unknown'),
+        ('h','Home'),
+        ('w','Work'),
+        ('m','Mobile'),
+        ('o','Other'),
+    )
+
+    TYPE_CHOICES = (
+        ('u','Unknown'),
+        ('v','Voice'),
+        ('m','Voice Mail'),
+        ('p','Pager'),
+        ('f','Fax'),
+    )
+
+    CONTACT_CHOICES = (
+        ('1','1'),
+        ('2','2'),
+        ('3','3'),
+    )
+
+    given = models.CharField(max_length=20, blank=True)
+    middle = models.CharField(max_length=20, blank=True)
+    family = models.CharField(max_length=20, blank=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES,
                             default='a', radio_admin=True)
     date_joined = models.DateField(default=date(1900, 01, 01))
     has_key = models.BooleanField(default=False)
     job = models.ForeignKey(Job)
+    
     accounts = models.ManyToManyField(Account, blank=True, null=True)
     account = models.ForeignKey(Account, verbose_name='Primary Account',
                                 related_name='accounts',blank=True, null=True)
+    
     address = models.ForeignKey(Address, blank=True, null=True)
-    contact_by = models.CharField(maxlength=1, choices=CONTACT_PREF,
+    contact_by = models.CharField(max_length=1, choices=CONTACT_PREF,
                                 default='e', radio_admin=True)
-    emails = models.ManyToManyField(Email, blank=True, null=True)    
-    email = models.ForeignKey(Email, verbose_name='Prefered Email Address',
-                                related_name='emails', blank=True, null=True)
-    phones = models.ManyToManyField(Phone, blank=True, null=True)    
-    phone = models.ForeignKey(Phone, verbose_name='Prefered Phone Number',
-                                related_name='phones', blank=True, null=True)
+    prefered_phone = models.CharField(max_length=1, choices=CONTACT_CHOICES,
+                                        default='1',)
+    prefered_email = models.CharField(max_length=1, choices=CONTACT_CHOICES,
+                                        default='1',)
 
-    def __str__(self):
+    phone_1 = models.PhoneNumberField(verbose_name='Phone 1', 
+                                    null=True, blank=True)
+    phone_1_ext = models.PositiveIntegerField(max_length=5,
+                                            verbose_name='Phone 1 Ext',
+                                            null=True, blank=True,)
+    phone_1_loc = models.CharField(max_length=1,
+                                    verbose_name='Phone 1 Location',
+                                    default='u', choices=LOCATION_CHOICES)
+    phone_1_type = models.CharField(max_length=1, verbose_name='Phone 1 Type',
+                                    default='u',choices=TYPE_CHOICES,)
+    phone_1_pub = models.BooleanField(default=0, verbose_name='Phone 1 Publish')
+    phone_2 = models.PhoneNumberField(verbose_name='Phone 2', 
+                                    null=True, blank=True)
+    phone_2_ext = models.PositiveIntegerField(max_length=5,
+                                            verbose_name='Phone 2 Ext',
+                                            null=True, blank=True,)
+    phone_2_loc = models.CharField(max_length=1,
+                                    verbose_name='Phone 2 Location',
+                                    default='u', choices=LOCATION_CHOICES)
+    phone_2_type = models.CharField(max_length=1, verbose_name='Phone 2 Type',
+                                    default='u',choices=TYPE_CHOICES,)
+    phone_2_pub = models.BooleanField(default=0, verbose_name='Phone 2 Publish')
+
+    phone_3 = models.PhoneNumberField(verbose_name='Phone 3', 
+                                    null=True, blank=True)
+    phone_3_ext = models.PositiveIntegerField(max_length=5,
+                                            verbose_name='Phone 3 Ext',
+                                            null=True, blank=True,)
+    phone_3_loc = models.CharField(max_length=1,
+                                    verbose_name='Phone 3 Location',
+                                    default='u', choices=LOCATION_CHOICES)
+    phone_3_type = models.CharField(max_length=1, verbose_name='Phone 3 Type',
+                                    default='u',choices=TYPE_CHOICES,)
+    phone_3_pub = models.BooleanField(default=0, verbose_name='Phone 3 Publish')
+
+    email_1 = models.EmailField(null=True, blank=True,)
+    email_1_loc = models.CharField(max_length=1,
+                                    verbose_name='Email 1 Location',
+                                    default='u', choices=LOCATION_CHOICES)
+    email_1_pub = models.BooleanField(default=0, verbose_name='Email 1 Publish')
+    email_2 = models.EmailField(null=True, blank=True,)
+    email_2_loc = models.CharField(max_length=1,
+                                    verbose_name='Email 2 Location',
+                                    default='u', choices=LOCATION_CHOICES)
+    email_2_pub = models.BooleanField(default=0, verbose_name='Email 2 Publish')
+    email_3 = models.EmailField(null=True, blank=True,)
+    email_3_loc = models.CharField(max_length=1,
+                                    verbose_name='Email 3 Location',
+                                    default='u', choices=LOCATION_CHOICES)
+    email_3_pub = models.BooleanField(default=0, verbose_name='Email 3 Publish')
+
+    def __unicode__(self):
         return '%s %s %s' % (self.given, self.middle, self.family)
     
     class Meta:
         ordering = ['given']
 
     class Admin:
-        list_display = ('__str__', 'account')
+        pass
+    '''
+        list_display = ('__str__', 'account',)
         fields = (
             (None, {
                 'fields': (('given', 'middle', 'family'),
                             ('user', 'password'), 'date_joined', 'status',
-                            'role', 'status', ('has_key', 'job')),
+                            'role', ('has_key', 'job', 'address')),
                 }),
             ('Accounts', {
                 'classes': 'collapse',
@@ -173,8 +174,17 @@ class Member(models.Model):
                 }),
             ('Contact', {
                 'classes': 'collapse',
-                'fields' : ('contact_by', ('email', 'emails'),
-                            ('phone', 'phones'),
-                            'address'),
+                'fields' : (('contact_by', 'prefered_email', 'prefered_phone'),
+                            ('email_1', 'email_1_loc', 'email_1_pub'),
+                            ('email_2', 'email_2_loc', 'email_2_pub'),
+                            ('email_3', 'email_3_loc', 'email_3_pub'),
+                            ('phone_1', 'phone_1_ext', 'phone_1_type',
+                                'phone_1_loc','phone_1_pub'),
+                            ('phone_2', 'phone_2_ext', 'phone_2_type',
+                                'phone_2_loc', 'phone_2_pub'),
+                            ('phone_3', 'phone_3_ext', 'phone_3_type',
+                                'phone_3_loc',  'phone_3_pub'),
+                            ),
                 }),
         )
+    '''
