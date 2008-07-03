11e23e7ab54c63817da4816be523046ed361c1d8
Gabriel Farrell <g@grrawr.com>
2008-07-03 03:33:16 +0000

    extra / in MEDIA_URL

--- mess/settings.py
+++ mess/settings.py
@@ -49,7 +49,7 @@ MEDIA_URL = PROJECT_URL + 'media/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = PROJECT_URL + '/admin/media/'
+ADMIN_MEDIA_PREFIX = PROJECT_URL + 'admin/media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '4w-cz*to=zc-gjoqtrf=pi2m54i@ghqlnsdq=2mq7e^8fbgh#w'
