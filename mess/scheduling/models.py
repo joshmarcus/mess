462cbd385db385eeeb3eaa7dd3372240dc932a8d
Gabriel Farrell <g@grrawr.com>
2008-07-10 23:57:55 +0000

    initial thoughts on scheduling models

--- mess/scheduling/models.py
+++ mess/scheduling/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Job(models.Model):
+class ShiftType(models.Model):
     name = models.CharField(max_length=40, unique=True)
 
     def __unicode__(self):
@@ -8,6 +8,21 @@ class Job(models.Model):
     
     class Meta:
         ordering = ['name']
+
+    class Admin:
+        pass
+
+class Shift(models.Model):
+    type = models.ForeignKey(ShiftType)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    completed = models.BooleanField()
+
+    def __unicode__(self):
+        return self.type.name
+    
+    class Meta:
+        ordering = ['start_time']
 
     class Admin:
         pass
