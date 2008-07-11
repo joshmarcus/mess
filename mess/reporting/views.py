cf2f6a352fc9a73c02efa3d2a0b57e609c95041a
Digger Vermont <digVer@verizon.net>
2008-07-11 17:33:33 +0000

    Import some modules.\nCreate a basic view for the 'Daily Report'.

--- mess/reporting/views.py
+++ mess/reporting/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+def daily_report(request):
+    """Page to reconcile the day's transactions."""
+    context = {}
+    return render_to_response('reporting/daily_report.html', context,
+                                context_instance=RequestContext(request))
+
