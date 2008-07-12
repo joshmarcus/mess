9463a262d183da9ef845b73853394c7e51d85ea8
Digger Vermont <digVer@verizon.net>
2008-07-12 22:38:26 +0000

    Point the reporting url to the daily report for the moment rather than going nowhere.

--- mess/reporting/urls.py
+++ mess/reporting/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('mess.reporting.views',
+    url(r'', 'daily_report', name='reports'),
     url(r'^daily_report$', 'daily_report', name='daily_report'),
 )
