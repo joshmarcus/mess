be74e3a86a10f9b69628b1e358fcbb6c639fb718
Digger Vermont <digVer@verizon.net>
2008-07-24 20:17:40 +0000

    Add slashes to a few urls. Add urls for close_close.  There must be a cleaner or dryer whay to do it.

--- mess/accounting/urls.py
+++ mess/accounting/urls.py
@@ -2,10 +2,19 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('mess.accounting.views',
     (r'^thanks$','thanks'),
-    url(r'^transaction$', 'transaction_form', name='transaction'),
-    #(r'^member_transaction$', 'member_transaction'),
-    #(r'^staff_transaction$', 'staff_transaction'),
+    url(r'^transaction/$', 'transaction_form', name='transaction'),
     url(r'^cashier$', 'cashier', name='cashier'),
-    url(r'^close_out', 'close_out', name='close_out'),
+    url(r'^close_out/$', 'close_out', name='close_out'),
+    
+    url(r'^close_out/checks/$', 'close_out',
+        {'type': 'K'}, name='close_out_checks' ),
+    url(r'^close_out/money_orders/$', 'close_out', 
+        {'type': 'M'}, name='close_out_money_orders'),
+    url(r'^close_out/credit/$', 'close_out',
+        {'type': 'C'}, name='close_out_credit'),
+    url(r'^close_out/ebt/$', 'close_out',
+        {'type': 'F'}, name='close_out_ebt'),
+    url(r'^close_out/all/$', 'close_out', name='close_out'),
+    
     #(r'^get_accounts/(d{0,10})$', 'get_accounts'),
 )
