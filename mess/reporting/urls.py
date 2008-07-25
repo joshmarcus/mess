1e8efad97373fe5e4f4b1b36334ee17d18dd04f6
Digger Vermont <digVer@verizon.net>
2008-07-25 00:30:42 +0000

    Lots of changes gets several new views.

--- mess/reporting/urls.py
+++ mess/reporting/urls.py
@@ -1,6 +1,20 @@
+from datetime import date, timedelta
 from django.conf.urls.defaults import *
 
+date_today = date.today()
+date_yesterday = date_today - timedelta(days=1)
+date_tomorrow = date_today + timedelta(days=1)
+dict_summary_today = {'start_date': date_today,
+                    'end_date': date_tomorrow,
+}
+dict_summary_yesterday = {'start_date': date_yesterday,
+                    'end_date': date_today,
+}
+
 urlpatterns = patterns('mess.reporting.views',
-    url(r'', 'daily_report', name='reports'),
-    url(r'^daily_report$', 'daily_report', name='daily_report'),
+    url(r'^trans_summary/$', 'transaction_report', name='trans_summary'),
+    url(r'^summary_today/$', 'transaction_report', dict_summary_today,
+                            name='trans_summary_today',),
+    url(r'^summary_yesterday/$', 'transaction_report', dict_summary_yesterday,
+                            name='trans_summary_yesterday',),
 )
