dbac93fe2e45f320c7061756bc211b90a9bbe0ed
Will Dyson <will.dyson@gmail.com>
2008-07-29 20:08:27 +0000

    Implement crud views for tasks

--- mess/scheduling/urls.py
+++ mess/scheduling/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls.defaults import *
+from mess.scheduling.models import *
 
 # commented out until there are actual views to point at -- 
 # phantom urls wreak havoc with {% url %} and reverse()
 urlpatterns = patterns('mess.scheduling.views',
-    url(r'^monthly/(?P<month>.*)', 'monthly', name="staff-schedules"),
+    url(r'^monthly/', 'monthly', name="staff-schedules"),
 #    url(r'^weekly.html', 'weekly'),
 #    url(r'^daily.html', 'daily'),
 #    url(r'^job_list.html', 'job_list'),
+
+    url(r'^task/new/', 'add_task', name="add-task"),
+    url(r'^task/update/(?P<object_id>\d+)', 'update_task', name="update-task"),
+    url(r'^task/delete/(?P<object_id>\d+)', 'delete_task', name="del-task"),
 )
--- mess/scheduling/views.py
+++ mess/scheduling/views.py
@@ -1,16 +1,45 @@
-from datetime import datetime
+import datetime
 
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.core.urlresolvers import reverse
+from django.views.generic.create_update import *
 
 from mess.scheduling.models import *
 
-def monthly(request, month=None):
-    if month == None:
-        month = datetime.month
+#task crud
+task_dict =  {
+    'model': Task,
+    'login_required': True,
+}
+
+
+
+def add_task(request, **kwargs):
+    add_dict = dict(task_dict)
+    add_dict.update(kwargs)
+    return create_object(request, post_save_redirect=reverse("staff-schedules"), **add_dict)
+
+def update_task(request, **kwargs):
+    up_dict = dict(task_dict)
+    up_dict.update(kwargs)
+    return update_object(request, post_save_redirect=reverse("staff-schedules"), **up_dict)
+
+def delete_task(request, **kwargs):
+    del_dict = dict(task_dict)
+    del_dict.update(kwargs)
+    return delete_object(request, post_delete_redirect=reverse("staff-schedules"), **del_dict)
+
+
+
+def monthly(request, date=None):
+    if date == None:
+        date = datetime.date.today()
+    else:
+        date = datetime.datetime(date)
     
     context = {
-        'tasks': Task.objects.all()
+        'tasks': Task.objects.filter(deadline__year=date.year, deadline__month=date.month)
     }
     return render_to_response('scheduling/monthly.html', context,
                                 context_instance=RequestContext(request))
