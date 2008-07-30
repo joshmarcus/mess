5c8f880b5fb7ae6ffb02d97b6ccf14183b00faed
Gabriel Farrell <g@grrawr.com>
2008-07-30 01:57:42 +0000

    copied some template and view stuff from contact to profiles

--- mess/profiles/views.py
+++ mess/profiles/views.py
@@ -1,1 +1,36 @@
-# Create your views here.
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.template import RequestContext
+
+from mess.profiles.models import Address
+
+def address(request, id):
+    context = RequestContext(request)
+    context['address'] = get_object_or_404(Address, id=id)
+    template = get_template('contact/address.html')
+    return HttpResponse(template.render(context))
+
+def address_form(request, id=None):
+    context = RequestContext(request)
+    if id:
+        address = get_object_or_404(Address, id=id)
+    else:
+        address = None
+    if request.method == 'POST':
+        form = AddressForm(request.POST, instance=address)
+        referer = request.POST['referer']
+        if form.is_valid():
+            new_address = form.save()
+            if referer:
+                return HttpResponseRedirect(referer)
+            else:
+                return HttpResponseRedirect(reverse('address', 
+                        args=[new_address.id]))
+    else:
+        form = AddressForm(instance=address)
+    context['form'] = form
+    referer = request.META.get('HTTP_REFERER', '')
+    context['referer'] = referer
+    template = get_template('contact/address_form.html')
+    return HttpResponse(template.render(context))
