ce6479e2f236ebd16fdc59e9db2e08f2a25ff9d5
Will Dyson <will.dyson@gmail.com>
2008-08-24 22:27:04 +0000

    Add filterability on deadline in Admin

--- mess/scheduling/admin.py
+++ mess/scheduling/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from mess.scheduling.models import Job, Task, Timecard
 
+class TaskAdmin(admin.ModelAdmin):
+    list_filter = ['deadline']
+
 admin.site.register(Job)
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
 admin.site.register(Timecard)
