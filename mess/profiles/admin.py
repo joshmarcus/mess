e56d25e52e8d618560901e1e07f5e5811b205dd1
Gabriel Farrell <g@grrawr.com>
2008-09-16 03:50:32 +0000

    added admin for addresses, phones, emails

--- mess/profiles/admin.py
+++ mess/profiles/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from mess.profiles.models import UserProfile
+from mess.profiles import models
 
 class UserProfileAdmin(admin.ModelAdmin):
     pass
-admin.site.register(UserProfile, UserProfileAdmin)
+
+class AddressAdmin(admin.ModelAdmin):
+    pass
+
+class PhoneAdmin(admin.ModelAdmin):
+    pass
+
+class EmailAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.UserProfile, UserProfileAdmin)
+admin.site.register(models.Address, AddressAdmin)
+admin.site.register(models.Phone, PhoneAdmin)
+admin.site.register(models.Email, EmailAdmin)
