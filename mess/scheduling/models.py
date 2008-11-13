89d1c4871ab185771184ebe00f291f4ef5f9fa7d
Gabriel Farrell <g@grrawr.com>
2008-11-13 05:19:21 +0000

    a little cleanup in scheduling models

--- mess/scheduling/models.py
+++ mess/scheduling/models.py
@@ -3,6 +3,16 @@ import datetime
 from django.db import models
 from mess.membership.models import Account, Member
 
+JOB_TYPES = (
+    ('s','Staff'),
+    ('p','Paid'),
+    ('m','Member'),
+)
+RECURRENCE_UNITS = (
+    ('d', 'days'),
+    ('w', 'weeks'),
+    ('m', 'months'),
+)
 
 class Skill(models.Model):
     """
@@ -13,18 +23,13 @@ class Skill(models.Model):
     def __unicode__(self):
         return self.name
 
-JOB_CHOICES = (
-    ('s','Staff'),
-    ('p','Paid'),
-    ('m','Member'),
-)
 class Job(models.Model):
     """
     Job description / title
     """
     name = models.CharField(max_length=100, unique=True)
-    desc = models.TextField(blank=True)
-    type = models.CharField(max_length=1, choices=JOB_CHOICES, default='m')
+    description = models.TextField(blank=True)
+    type = models.CharField(max_length=1, choices=JOB_TYPES, default='m')
     freeze_days = models.IntegerField(default=7)
     hours_multiplier = models.IntegerField(default=1)
     skill_required = models.ForeignKey(Skill, blank=True, null=True, 
@@ -37,6 +42,43 @@ class Job(models.Model):
     
     class Meta:
         ordering = ['name']
+
+
+class Task(models.Model):
+    """
+    A task is a scheduled occurrence of a job (or occurences, if recurring), 
+    and has either a deadline or a start time.
+    """
+    job = models.ForeignKey(Job)
+    deadline = models.DateTimeField(null=True, blank=True)
+    start = models.DateTimeField(null=True, blank=True)
+    account = models.ForeignKey(Account, null=True, blank=True)
+    member = models.ForeignKey(Member, null=True, blank=True)
+    hours = models.DecimalField(max_digits=4, decimal_places=2)
+    recurrence_freq = models.IntegerField(default=0)
+    recurrence_unit = models.CharField(max_length=1, choices=RECURRENCE_UNITS, 
+            default='w')
+    
+    class Meta:
+        ordering = ['-deadline', 'start']
+    
+    def __unicode__(self):
+        return u"%s hrs of %s before %s" % (self.hours, self.job.name, 
+                self.deadline.date())
+
+
+class Timecard(models.Model):
+    """
+    Keep track of the time worked on a task (through assignment)
+    """
+    task = models.ForeignKey(Task)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    
+    def __unicode__(self):
+        work = self.end - self.start
+        return u"%s hrs of %s" % (work.seconds / 3600, self.task.job)
+
 
 #class RecurringShift(models.Model):
 #    """
@@ -51,39 +93,3 @@ class Job(models.Model):
 #    def __unicode__(self):
 #        return u"%s hrs of %s every %s weeks" % (self.hours, self.job.name, self.frequency)
 
-RECURRANCE_UNITS = (
-    ('d', 'days'),
-    ('w', 'weeks'),
-    ('m', 'months'),
-)
-
-class Task(models.Model):
-    """
-    A task is an instance of a job that occurs once
-    """
-    job = models.ForeignKey(Job)
-    deadline = models.DateTimeField(null=True, blank=True)
-    start = models.DateTimeField(null=True, blank=True)
-    account = models.ForeignKey(Account)
-    member = models.ForeignKey(Member)
-    hours = models.IntegerField()
-    recurrance_freq = models.IntegerField(default=0)
-    recurrance_unit = models.CharField(max_length=1, choices=RECURRANCE_UNITS, default='w')
-    
-    class Meta:
-        ordering = ['-deadline', 'start']
-    
-    def __unicode__(self):
-        return u"%s hrs of %s before %s" % (self.hours, self.job.name, self.deadline.date())
-
-class Timecard(models.Model):
-    """
-    Keep track of the time worked on a task (through assignment)
-    """
-    task = models.ForeignKey(Task)
-    start = models.DateTimeField()
-    end = models.DateTimeField()
-    
-    def __unicode__(self):
-        work = self.end - self.start
-        return u"%s hrs of %s" % (work.seconds / 3600, self.task.job)
