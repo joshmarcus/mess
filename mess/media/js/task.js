3dd1df0d66df1f55067d288f7c67f33f3725c1e3
Thomas Schreiber <tom@insatsu.us>
2008-11-13 04:26:43 +0000

    working sub-select, not super clean, but works'

--- mess/media/js/task.js
+++ mess/media/js/task.js
@@ -1,7 +1,7 @@
 // task.js
 
 function memberFilter(account_id) {
-  var sUrl = '?account=' + account_id;
+  var sUrl = '/accounting/cashier?account=' + account_id;
   var callback = {
     success: function(o) {
       var members = document.getElementById('id_member');
