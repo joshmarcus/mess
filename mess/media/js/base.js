716ed72879b19e6fe88a75847b663dd001dc0a0a
Mike Ball <mikedball@gmail.com>
2008-11-21 16:43:04 +0000

    added makeActive function to base.js to style the active local nav menu. will need to add init function to head of pages that use this

--- mess/media/js/base.js
+++ mess/media/js/base.js
@@ -175,3 +175,17 @@ function reset_form() {
     document.form.reset();
   }
 }
+
+//adds class="active" to active local nav item. anyone want to make this work on global nav?
+function makeActive() {
+	var urlPath = window.location.pathname;
+	var navList = document.getElementById("local-nav");
+	var navLinks = navList.getElementsByTagName("a");
+	for (var i = 0; i < navLinks.length; i++) {
+	    var navLink = navLinks[i];
+	    if (navLink.pathname == urlPath) {
+	        navLink.className = "active";
+	    }
+	}
+}
+
