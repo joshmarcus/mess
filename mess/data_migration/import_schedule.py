8499fe71151794b787af89d2e8099e9e2aa4008a
Will Dyson <will.dyson@gmail.com>
2008-11-30 15:26:28 +0000

    Handle type 2 schedule rows

--- mess/data_migration/import_schedule.py
+++ mess/data_migration/import_schedule.py
@@ -13,7 +13,7 @@ from scheduling.models import *
 from membership.models import *
 
 
-def make_job(row):
+def make_job(row, book):
     'minimally validate and create jobs from sheet rows'
     if row[0].ctype != xlrd.XL_CELL_NUMBER:
         print "rejecting row:"
@@ -32,8 +32,53 @@ def make_job(row):
     job.save()
     print job
 
-def make_task(row):
-    'minimally validate and load tasks'
+# if columns were addressable by label, this would collapse easily with the other func
+def make_task_fmt2(row, book):
+    'minimally validate and load format 2 tasks'
+    isoTimeFmt = "%Y-%m-%dT%H:%M"
+    
+    if row[0].ctype != xlrd.XL_CELL_NUMBER:
+        print "rejecting row: ", row
+        return
+
+    if row[1].ctype != xlrd.XL_CELL_DATE:
+        print "format 2 handler got format 1 row" 
+        return
+
+    job = Job.objects.get(id = row[0].value)
+
+    start_day = datetime.datetime(*xlrd.xldate_as_tuple(row[1].value, book.datemode))
+    start_time_tup = xlrd.xldate_as_tuple(row[2].value, book.datemode)
+    print start_time_tup
+    start_time = datetime.time(*start_time_tup[3:]) #slice out just the time part
+
+    start = datetime.datetime.combine(start_day, start_time)
+
+    task = Task(
+            job = job,
+            start = start,
+            hours = str(row[4].value),
+            recurrence_unit = row[6].value.lower(),
+            recurrence_freq = row[7].value,
+            )
+    try:
+        member = Member.objects.get(user__username = row[8].value)
+        account = Account.objects.get(name = row[9].value)
+        task.member = member
+        task.account = account
+    except Member.DoesNotExist, Account.DoesNotExist:
+        pass
+
+    try:
+        task.deadline = datetime.datetime.strptime(row[3].value, isoTimeFmt)
+    except:
+        pass
+    
+    task.save()
+    print task
+
+def make_task_fmt1(row, book):
+    'minimally validate and load format 1 tasks'
     isoTimeFmt = "%Y-%m-%dT%H:%M"
     
     if row[0].ctype != xlrd.XL_CELL_NUMBER:
@@ -41,7 +86,7 @@ def make_task(row):
         return
 
     if row[1].ctype != xlrd.XL_CELL_TEXT:
-        print "ignoring fmt2 row" 
+        print "format 1 handler got format 2 row" 
         return
 
     job = Job.objects.get(id = row[0].value)
@@ -70,17 +115,20 @@ def make_task(row):
     task.save()
     print task
 
-def dispatch_rows(sheet):
+def dispatch_rows(sheet, book):
     print "processing %s" % sheet.name
         
     if sheet.name == u"Jobs":
         handler = make_job
     if sheet.name.find('Shift Sch') >= 0:
-        handler = make_task
+        if sheet.name.find('Fmt1') >= 0:
+            handler = make_task_fmt1
+        else:
+            handler = make_task_fmt2
 
     for i in range(1, sheet.nrows):
         row = sheet.row(i)
-        handler(row)
+        handler(row, book)
 
 def main():
     'Open Workbooks, dispatch to handlers'
@@ -92,7 +140,7 @@ def main():
     print "opening %s\n" % bookname
     book = xlrd.open_workbook(bookname)
     for sheet in book.sheets():
-        dispatch_rows(sheet)
+        dispatch_rows(sheet, book)
 
 if __name__ == "__main__":
     sys.exit(main())
