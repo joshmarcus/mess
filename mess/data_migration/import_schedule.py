996eb2423f0f12e9dd9dbc573a5b4b2fb7d99124
Will Dyson <will.dyson@gmail.com>
2008-12-12 21:32:06 +0000

    Improve date parsing, error handleing and error output

--- mess/data_migration/import_schedule.py
+++ mess/data_migration/import_schedule.py
@@ -8,6 +8,7 @@ setup_environ(settings)
 import xlrd
 from xlrd import empty_cell
 import datetime
+from dateutil.parser import parse
 
 from scheduling import models as sm
 from membership import models as mm
@@ -45,9 +46,13 @@ def make_task_fmt2(row, book):
     job = sm.Job.objects.get(id = row[0].value)
 
     start_day = datetime.datetime(*xlrd.xldate_as_tuple(row[1].value, book.datemode))
-    start_time_tup = xlrd.xldate_as_tuple(row[2].value, book.datemode)
-    start_time = datetime.time(*start_time_tup[3:]) #slice out just the time part
 
+    if row[2].ctype == xlrd.XL_CELL_DATE:
+        start_time_tup = xlrd.xldate_as_tuple(row[2].value, book.datemode)
+        start_time = datetime.time(*start_time_tup[3:]) #slice out just the time part
+    else:
+        start_time = parse(row[2].value).time()
+    
     start = datetime.datetime.combine(start_day, start_time)
 
     task = sm.Task(
@@ -63,15 +68,17 @@ def make_task_fmt2(row, book):
     if acct_name != "tba" and acct_name != "tbd" and acct_name != "TBD" and acct_name != "":
         try:
             account = mm.Account.objects.get(name = acct_name)
-            members = account.members.filter(user__first_name = mem_name)
-            if members.count() == 0:
-                raise mm.Member.DoesNotExist
-            task.member = members[0]
-            task.account = account
-        except mm.Account.DoesNotExist:
-            print "No such account: %s\n" % acct_name
-        except mm.Member.DoesNotExist:
-            print "No matching member %s on acct: %s\n" % (mem_name, account)
+        except mm.Account.DoesNotExist, e:
+            e.account = acct_name
+            raise e
+        members = account.members.filter(user__first_name = mem_name)
+        if members.count() == 0:
+            e = mm.Member.DoesNotExist()
+            e.account = str(account)
+            e.member = mem_name
+            raise e
+        task.member = members[0]
+        task.account = account
     
     task.save()
 #    print task
@@ -102,15 +109,17 @@ def make_task_fmt1(row, book):
     if acct_name != "tba" and acct_name != "tbd" and acct_name != "TBD" and acct_name != "":
         try:
             account = mm.Account.objects.get(name = acct_name)
-            members = account.members.filter(user__first_name = mem_name)
-            if members.count() == 0:
-                raise mm.Member.DoesNotExist
-            task.member = members[0]
-            task.account = account
-        except mm.Account.DoesNotExist:
-            print "No such account: %s\n" % acct_name
-        except mm.Member.DoesNotExist:
-            print "No matching member %s on acct: %s\n" % (mem_name, account)
+        except mm.Account.DoesNotExist, e:
+            e.account = acct_name
+            raise e
+        members = account.members.filter(user__first_name = mem_name)
+        if members.count() == 0:
+            e = mm.Member.DoesNotExist()
+            e.account = str(account)
+            e.member = mem_name
+            raise e
+        task.member = members[0]
+        task.account = account
     
     task.save()
 #    print task
@@ -127,8 +136,19 @@ def dispatch_rows(sheet, book):
             handler = make_task_fmt2
 
     for i in range(1, sheet.nrows):
-        row = sheet.row(i)
-        handler(row, book)
+        row_idx = i + 1
+        try:
+            row = sheet.row(i)
+            handler(row, book)
+        except sm.Job.DoesNotExist, e:
+            print "FATAL  %s: Sheet %s, row %d\n" % (e, sheet.name, row_idx)
+        except sm.Account.DoesNotExist, e:
+            print "Partial  Unknown account %s: Sheet %s, row %d\n" % (e.account, sheet.name, row_idx)
+        except sm.Member.DoesNotExist, e:
+            print "Partial  No member %s in account %s: Sheet %s, row %d\n" % (e.member, e.account, sheet.name, row_idx)
+        except:
+            print "TOTAL FAIL!!! Sheet %s, row %d\n" % (sheet.name, row_idx)
+            raise
 
 def main():
     'Open Workbooks, dispatch to handlers'
