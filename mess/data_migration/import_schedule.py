6be03ac99e2cab6303b6e125262ca778f7726172
Will Dyson <will.dyson@gmail.com>
2009-01-15 22:01:05 +0000

    Adjust sched import for new models

--- mess/data_migration/import_schedule.py
+++ mess/data_migration/import_schedule.py
@@ -10,17 +10,21 @@ import xlrd
 from xlrd import empty_cell
 import datetime
 from dateutil.parser import parse
+from django.db import transaction
 
-sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
 from scheduling import models as sm
 from membership import models as mm
+
+def zip_row(row, headers):
+    rowItems = [col for col in row]
+    return dict(zip(headers, rowItems))
 
 class DateParseError(Exception):
     pass
 
 
-def make_job(row, book):
+def make_job(row, zipRow, book):
     'minimally validate and create jobs from sheet rows'
     if row[0].ctype != xlrd.XL_CELL_NUMBER:
         return
@@ -37,22 +41,18 @@ def make_job(row, book):
     job.save()
     print job
 
-# if columns were addressable by label, this would collapse easily with the other func
-def make_task_fmt2(row, book):
-    'minimally validate and load format 2 tasks'
+def get_start_time1(row, zipRow, book):
     isoTimeFmt = "%Y-%m-%dT%H:%M"
-    
-    if row[0].ctype != xlrd.XL_CELL_NUMBER:
-        return
+    try:
+        start = datetime.datetime.strptime(row[1].value, isoTimeFmt)
+    except:
+        e = DateParseError()
+        e.time = row[1].value
+        raise e
+    return start
 
-    if row[1].ctype != xlrd.XL_CELL_DATE:
-        print "format 2 handler got format 1 row" 
-        return
-
-    job = sm.Job.objects.get(id = row[0].value)
-
+def get_start_time2(row, zipRow, book):
     start_day = datetime.datetime(*xlrd.xldate_as_tuple(row[1].value, book.datemode))
-
     try:
         if row[2].ctype == xlrd.XL_CELL_DATE:
             start_time_tup = xlrd.xldate_as_tuple(row[2].value, book.datemode)
@@ -65,65 +65,34 @@ def make_task_fmt2(row, book):
         raise e
 
     start = datetime.datetime.combine(start_day, start_time)
+    return start
 
-    task = sm.Task(
-            job = job,
-            time = start,
-            hours = str(row[4].value),
-            frequency = row[6].value.lower(),
-            interval = row[7].value,
-            )
-
-    acct_name = row[9].value.strip()
-    mem_name = row[8].value.strip()
-    if acct_name != "tba" and acct_name.lower() != "tbd" and acct_name.lower() != "t-b-d" and acct_name != "":
-        try:
-            account = mm.Account.objects.get(name = acct_name)
-        except mm.Account.DoesNotExist, e:
-            e.account = acct_name
-            raise e
-        members = account.members.filter(user__first_name = mem_name)
-        if members.count() == 0:
-            members = account.members.filter(user__first_name__startswith = mem_name)
-        if members.count() == 0:
-            e = mm.Member.DoesNotExist()
-            e.account = str(account)
-            e.member = mem_name
-            raise e
-        task.member = members[0]
-        task.account = account
-    
-    task.save()
-#    print task
-
-def make_task_fmt1(row, book):
-    'minimally validate and load format 1 tasks'
-    isoTimeFmt = "%Y-%m-%dT%H:%M"
-    
+def make_task(row, zipRow, book):
     if row[0].ctype != xlrd.XL_CELL_NUMBER:
         return
 
-    if row[1].ctype != xlrd.XL_CELL_TEXT:
-        print "format 1 handler got format 2 row" 
-        return
+    if row[1].ctype == xlrd.XL_CELL_DATE:
+        start_func = get_start_time2
+    else:
+        start_func = get_start_time1
 
-    job = sm.Job.objects.get(id = row[0].value)
-    try:
-        start = datetime.datetime.strptime(row[1].value, isoTimeFmt)
-    except:
-        e = DateParseError()
-        e.time = row[1].value
-        raise e
+
+    recur_rule = sm.RecurRule(
+            frequency = zipRow["d/w/mo"].value.lower(),
+            interval = zipRow["Cycle"].value,
+    )
+    recur_rule.save()
 
     task = sm.Task(
-            job = job,
-            time = start,
-            hours = str(row[3].value),
-            frequency = row[5].value.lower(),
-            interval = row[6].value,
-            )
-    acct_name = row[8].value.strip()
-    mem_name = row[7].value.strip()
+            job = sm.Job.objects.get(id = zipRow["FK-Job"].value),
+            time = start_func(row, zipRow, book),
+            hours = str(zipRow["Hrs"].value),
+            recur_rule = recur_rule,
+    )
+    task.save()
+
+    acct_name = zipRow["Account"].value.strip()
+    mem_name = zipRow["Member Name"].value.strip()
     if acct_name != "tba" and acct_name.lower() != "tbd" and acct_name.lower() != "t-b-d" and acct_name != "":
         try:
             account = mm.Account.objects.get(name = acct_name)
@@ -138,31 +107,34 @@ def make_task_fmt1(row, book):
             e.account = str(account)
             e.member = mem_name
             raise e
-        task.member = members[0]
-        task.account = account
+        worker = sm.Worker(
+                task = task,
+                member = members[0],
+                account = account,
+        )
+        worker.save()
     
-    task.save()
-#    print task
-
+@transaction.commit_on_success
 def dispatch_rows(sheet, book):
     print "processing %s\n\n" % sheet.name
     errors = 0
     if sheet.name == u'Jobs':
         handler = make_job
     elif sheet.name.find('Shift Sch') >= 0:
-        if sheet.name.find('Fmt1') >= 0:
-            handler = make_task_fmt1
-        else:
-            handler = make_task_fmt2
+        handler = make_task
     else:
         print "Aborting %s: Unknown sheet type" % sheet.name
         return errors
+        
+    header_row = sheet.row(0)
+    headers = [str(col.value) for col in header_row]
 
     for i in range(1, sheet.nrows):
         row_idx = i + 1
         try:
             row = sheet.row(i)
-            handler(row, book)
+            zipRow = zip_row(row, headers)
+            handler(row, zipRow, book)
         except sm.Job.DoesNotExist, e:
             errors += 1
             print "FATAL  %s: Sheet %s, row %d\n" % (e, sheet.name, row_idx)
@@ -182,6 +154,7 @@ def dispatch_rows(sheet, book):
 
 def main():
     'Open Workbooks, dispatch to handlers'
+    sys.stdout = codecs.getwriter('utf8')(sys.stdout)
     if len(sys.argv) < 2 or sys.argv[1] == "--help":
         print "usage: %s <xl workbook>" % sys.argv[0]
         return 0
