16c328a6f56c619aeadab3f3b8862ddbfb4a9faa
Anna Headley <anna3lc@gmail.com>
2009-09-04 02:20:42 +0000

    welcome page now shows latest updates from the mariposa rss feed and some account info on right sidebar closes #11

--- mess/core/views.py
+++ mess/core/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.template.loader import get_template
+import feedparser
 
 def welcome(request):
+    MAX_ENTRIES = 5    # maximum number of rss items to show on welcome page
     context = RequestContext(request)
-    # TODO: stick feedparser stuff in context['rss_list']
+
+    # just passing the entries, so we don't have too many
+    feed = feedparser.parse("http://www.mariposa.coop/?feed=rss2")
+    if (len(feed.entries) > MAX_ENTRIES):
+        entries = feed.entries[:MAX_ENTRIES]
+    else: 
+        entries = feed.entries
+    context['rss_entries'] = entries
+
     template = get_template('welcome.html')
     return HttpResponse(template.render(context))
