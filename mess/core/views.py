08258c71bfd4d30aa7dda589cb7a3b79d2b83c65
Anna Headley <anna3lc@gmail.com>
2009-10-02 02:00:05 +0000

    starting on the new rss code but it's not working yet

--- mess/core/views.py
+++ mess/core/views.py
@@ -8,6 +8,7 @@ def welcome(request):
     context = RequestContext(request)
 
     # just passing the entries, so we don't have too many
+    #entries = _parseUrl("http://www.mariposa.coop/?feed=rss2")
     #feed = feedparser.parse("http://www.mariposa.coop/?feed=rss2")
     #if (len(feed.entries) > MAX_ENTRIES):
     #    entries = feed.entries[:MAX_ENTRIES]
@@ -18,3 +19,28 @@ def welcome(request):
 
     template = get_template('welcome.html')
     return HttpResponse(template.render(context))
+
+'''
+this code isn't working.  doesn't know what socket is.... 
+def _parseUrl(url):
+    # copied from http://mxm-mad-science.blogspot.com/2009/01/small-trick-for-socket-timouts-in-plone.html
+    # "Fetch the url"
+    orig_timout = socket.getdefaulttimeout() # get original timeout
+    socket.setdefaulttimeout(20)             # set to 20 seconds
+    try:
+        result = feedparser.parse(url)['entries']
+    except:
+        result = []
+    socket.setdefaulttimeout(orig_timout)    # set back to original
+    endresult = []
+    for r in result:
+        r2 = {}
+        r2.update(r)
+        endresult.append(r2)
+    # sort for latest first
+    decorated = [(r['updated_parsed'], r) for r in endresult]
+    decorated.sort()
+    decorated.reverse()
+    sorted = [d[-1] for d in decorated]
+    return sorted
+'''
