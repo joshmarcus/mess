4c4475d6740a9594f1b682764438f41426add31e
Gabriel Farrell <g@grrawr.com>
2009-11-17 04:21:04 +0000

    Updated forms for new Django csrf middleware.  Refs #252.

--- mess/settings.py
+++ mess/settings.py
@@ -67,6 +67,7 @@ TEMPLATE_LOADERS = (
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.middleware.doc.XViewMiddleware',
     'mess.core.middleware.UserPassesTestMiddleware',
