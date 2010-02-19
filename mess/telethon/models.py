2cb0a067ed3f9b365ec9f9b8dffe1ff7161d516b
Gabriel Farrell <g@grrawr.com>
2010-02-19 01:56:46 +0000

    Added telethon app with models filled in as discussed

---
+++ mess/telethon/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from mess.accounting.models import Transaction
+
+class Call(models.Model):
+    caller = models.ForeignKey(User)
+    callee = models.ForeignKey(Member)
+    note = models.TextField()
+    date = models.DateTimeField()
+    do_not_call = models.BooleanField()
+    pledge_amount = models.DecimalField(max_digits=8, decimal_places=2,
+        default=0, blank=True)
+    # XXX: other pledge stuff?
+
+LOAN_TERMS = (
+    ('a', 'Loan Term A'),
+    ('b', 'Loan Term B'),
+    ('c', 'Loan Term C'),
+)
+
+class Loan(models.Model):
+    term = models.CharField(max_length=1, choices=LOAN_TERMS,
+            default='a')
+    transaction = models.ForeignKey(Transaction)
+
