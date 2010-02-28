b418b7147638435d1cb29d0136bddf02fae3261d
Gabriel Farrell <g@grrawr.com>
2010-02-28 22:16:52 +0000

    Fundraising add member url, view, template. Refs #280

--- mess/telethon/urls.py
+++ mess/telethon/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('mess.telethon.views',
     url(r'^$', 'index', name='telethon-index'),
+    url(r'^(\w+)$', 'member', name='telethon-member'),
 )
--- mess/telethon/views.py
+++ mess/telethon/views.py
@@ -1,9 +1,19 @@
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 from django.template.loader import get_template
 
 def index(request):
     context = RequestContext(request)
     template = get_template('fundraising/index.html')
+    return HttpResponse(template.render(context))
+
+def member(request, username):
+    user = get_object_or_404(User, username=username)
+    member = user.get_profile()
+    context = RequestContext(request)
+    context['member'] = member
+    template = get_template('fundraising/member.html')
     return HttpResponse(template.render(context))
 
