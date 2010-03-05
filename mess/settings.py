0766a8479b9bfd0ffdb6abe3f79f5fde9baa493d
Gabriel Farrell <g@grrawr.com>
2010-03-05 16:00:19 +0000

    Staff should have access to fundraising.  Refs #280

--- mess/settings.py
+++ mess/settings.py
@@ -131,7 +131,8 @@ USER_PASSES_TEST_URLS = (
     (r'^/logout/$', None),
     (r'^/media/', None),
     (r'^/passwordreset/', None),
-    (r'^/fundraising/', lambda u: u.has_perm('telethon.can_fundraise')),
+    (r'^/fundraising/', lambda u: u.has_perm('telethon.can_fundraise') or 
+        u.is_staff),
     (r'', lambda u: u.is_staff),
 )
 
