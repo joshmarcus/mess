b5c413513d1f3da0027dd4e2373e2899d1ca7eb3
Mike Ball <mikedball@gmail.com>
2010-08-23 21:02:23 +0000

    Fine-tuned sub_nav behavior and styles

--- mess/media/js/base.js
+++ mess/media/js/base.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
 	
 	//hidd sub_nav on document.ready and display it when span.more is clicked
 	$('.sub_nav').hide();
-	$("#main_nav ul li .more").click(function() {
-    $(this).toggleClass("open");
+	$('#main_nav ul li .more').click(function() {
+    $(this).toggleClass('open');
+		$(this).parent().toggleClass('clicked');
     $(this).parent().next('.sub_nav').slideToggle(200);
 	 });
 	 $('body').click(function() {
