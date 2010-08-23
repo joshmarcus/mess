eaba05d583858770a490bfbbeea428cb060bd9d2
Mike Ball <mikedball@gmail.com>
2010-08-23 20:51:04 +0000

    Added ui-2.0 js and base.js

---
+++ mess/media/js/base.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	
+	//hidd sub_nav on document.ready and display it when span.more is clicked
+	$('.sub_nav').hide();
+	$("#main_nav ul li .more").click(function() {
+    $(this).toggleClass("open");
+    $(this).parent().next('.sub_nav').slideToggle(200);
+	 });
+	 $('body').click(function() {
+	   $('.sub_nav').hide();
+	 });
+	
+});
