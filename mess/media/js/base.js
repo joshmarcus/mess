8dd58fe5f444765af7f88ea9ccfecf3674f3bda3
Mike Ball <mikedball@gmail.com>
2010-08-26 19:19:10 +0000

    IE users are now presented animated alert asking them to download FF or Chrome

--- mess/media/js/base.js
+++ mess/media/js/base.js
@@ -3,6 +3,17 @@ function focusLogin() {
 	$('input#id_username').focus();
 }
 
+//Detect and alert IE users to download firefox or chrome
+function detectIE() {
+	var message = '<div id="browser_message"><p>You are using Internet Explorer. MESS works best with a modern browser. Please download a free copy of <a href="http://www.mozilla.com/en-US/">Mozilla Firefox</a> or <a href="http://www.google.com/chrome">Google Chrome</a>.</p></div>';
+	if ($.browser.msie) {
+		window.setTimeout(function () {
+			$(message).insertBefore('#container').hide().slideDown('fast');
+		}, 500);
+	}
+}
+
+//stuff automaticall run on pageload
 $(document).ready(function() {	
 	//hidd sub_nav on document.ready and display it when span.more is clicked
 	$('.sub_nav').hide();
