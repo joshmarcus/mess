639bd292f0b0942e15b1808aa4a268ce0b25fc61
Paul Dexter [xilotl] <krubokrubo@gmail.com>
2011-06-20 21:39:43 -0400

    gh-316 gh-329 gh-328 database updates

--- mess/data_migration/equity_columns.sql
+++ mess/data_migration/equity_columns.sql
@@ -1,5 +1,5 @@
-alter table membership_member add column "equity_held" decimal NOT NULL DEFAULT 0;
-alter table membership_member add column "equity_due" decimal NOT NULL DEFAULT 0;
-alter table membership_member add column "equity_increment" decimal NOT NULL DEFAULT 25;
+alter table membership_member add column "equity_held" numeric(8,2) NOT NULL DEFAULT 0.00;
+alter table membership_member add column "equity_due" numeric(8,2) NOT NULL DEFAULT 0.00;
+alter table membership_member add column "equity_increment" numeric(8,2) NOT NULL DEFAULT 25.00;
 alter table membership_account add column "shared_address" bool NOT NULL DEFAULT 0;
 alter table accounting_transaction add column "is4c_timestamp" datetime; 
