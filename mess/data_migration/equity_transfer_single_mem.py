e3ed91fbb5765098a9c416212ab917840220c813
Paul Dexter [mess] <krubokrubo@gmail.com>
2011-09-26 21:58:24 -0400

    add Mike and Anna's script to transfer equity for single member accounts #330

---
+++ mess/data_migration/equity_transfer_single_mem.py
@@ -0,0 +1,35 @@
+'''
+Says Anna:
+    This don't work 'cause:
+        - trans1 subtracts account.deposit
+        - so when trans2 tries to add it, it's zero.
+    Doh.
+Says Paul: fixed
+'''
+from os.path import dirname, abspath
+import sys
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+import settings
+from django.core.management import setup_environ
+setup_environ(settings)
+
+from mess.membership import models
+from mess.accounting import models as a_models
+accts = models.Account.objects.all()
+entered_by = models.Member.objects.get(user__id=259).user #This is Paul Dexter
+for acct in accts:
+    if acct.active_member_count == 1:
+        print "Before Transfer: " + repr(acct.name), acct.deposit, repr(acct.members.active()[0]), acct.members.active()[0].equity_held
+        old_deposit = acct.deposit
+        trans1 = a_models.Transaction.objects.create(
+                        note='transferring equity from account',
+                        account=acct,
+                        entered_by=entered_by, purchase_type='O',
+                        purchase_amount=-old_deposit)
+        trans2 = a_models.Transaction.objects.create(
+                        note='transferring equity to member',
+                        account=acct,
+                        member=acct.members.active()[0],
+                        entered_by=entered_by, purchase_type='O',
+                        purchase_amount=old_deposit)
+        print "After Transfer: " + repr(acct.name), acct.deposit, repr(acct.members.active()[0]), acct.members.active()[0].equity_held
