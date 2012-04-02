5b35d5d5ae5a960260d0600ac0f3064d8af91cf0
Gabriel Farrell <g@grrawr.com>
2012-04-02 15:25:41 -0400

    Button can post to existing passwordreset
    Post to the existing auth view for password reset
    and drop our new view. The error handling isn't
    quite as good but it'll work as long as everyone
    has an email address. Closes #379

--- mess/core/urls.py
+++ mess/core/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('mess.core.views',
     url(r'^$', 'welcome', name='welcome'),
-    url(r'^_passreset$', 'pass_reset', name='pass_reset'),
 )
